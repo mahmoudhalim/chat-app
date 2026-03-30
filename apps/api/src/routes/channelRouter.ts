5583e25a60f13c825b9e56c3218886dfe1e503ce
mahmoudhalim <mahmoudahalim0@gmail.com>
2026-03-30 20:08:11 +0200

    fix(api): align express routing with REST standards
    - Mount serverRouter correctly on '/api/servers' instead of '/channels'
    - Move channel creation and fetching to nested server routes
    ('/api/servers/:serverId/channels')
    - Update delete channel route to flat path ('/api/channels/:id')

--- apps/api/src/routes/channelRouter.ts
+++ apps/api/src/routes/channelRouter.ts
@@ -1,15 +1,11 @@
 import { Router } from "express";
 import channelController from "@controllers/channelController";
 import requireAuth from "@middlewares/requireAuth";
-import validateBody from "@middlewares/validateBody";
-import { createChannelSchema } from "@validators/channelValidator";
 
 const channelRouter = Router();
 
 channelRouter.use(requireAuth);
 
-channelRouter.post("/server/:serverId", validateBody(createChannelSchema), channelController.createChannel);
-channelRouter.get("/server/:serverId", channelController.getServerChannels);
 channelRouter.delete("/:id", channelController.deleteChannel);
 
 export default channelRouter;
--- apps/api/src/routes/serverRouter.ts
+++ apps/api/src/routes/serverRouter.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import serverController from "@controllers/serverController";
+import channelController from "@controllers/channelController";
 import requireAuth from "@middlewares/requireAuth";
 import validateBody from "@middlewares/validateBody";
 import { createServerSchema, joinServerSchema } from "@validators/serverValidator";
+import { createChannelSchema } from "@validators/channelValidator";
 
 const serverRouter = Router();
 
@@ -13,5 +15,8 @@ serverRouter.get("/:id", serverController.getServerById);
 serverRouter.post("/join", requireAuth, validateBody(joinServerSchema), serverController.joinServer);
 serverRouter.post("/:id/leave", requireAuth, serverController.leaveServer);
 serverRouter.delete("/:id", requireAuth, serverController.deleteServer);
+
+serverRouter.post("/:serverId/channels", requireAuth, validateBody(createChannelSchema), channelController.createChannel);
+serverRouter.get("/:serverId/channels", channelController.getServerChannels);
 
 export default serverRouter;
